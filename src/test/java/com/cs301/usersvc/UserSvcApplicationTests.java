56a74484561f0d653eda63400483fddb45c7cc8b
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-02-23 21:44:02 +0800

    chore: add scaffolding

---
+++ src/test/java/com/cs301/usersvc/UserSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs301.usersvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
