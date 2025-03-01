0facc896e6046af24ecda4fa1dda889c2a470167
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-01 22:40:57 +0800

    feat: add models

---
+++ src/main/java/com/cs301/crm/models/AgentUser.java
@@ -0,0 +1,31 @@
+package com.cs301.crm.models;
+
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AgentUser implements UserDetails {
+
+    private final User user;
+
+    public AgentUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_AGENT"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
