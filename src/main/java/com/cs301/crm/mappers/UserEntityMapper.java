7a549457ee9d2dda239bed533c7f1b59850b25f0
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-04 23:02:32 +0800

    refactor: refactor mapper to be more lean

--- src/main/java/com/cs301/crm/mappers/UserEntityMapper.java
+++ src/main/java/com/cs301/crm/mappers/UserEntityMapper.java
@@ -2,11 +2,8 @@ package com.cs301.crm.mappers;
 
 import com.cs301.crm.dtos.requests.CreateUserRequestDTO;
 import com.cs301.crm.models.UserEntity;
-import com.cs301.crm.models.UserRole;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
-import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
