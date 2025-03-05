9164c28f156ca613a0555cd739b593d0856f71c8
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-05 22:13:35 +0800

    feat: add aws helpers

--- src/main/java/com/cs301/crm/utils/AwsUtil.java
+++ src/main/java/com/cs301/crm/utils/AwsUtil.java
@@ -1,4 +1,44 @@
 package com.cs301.crm.utils;
 
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+
+/**
+ * @author: gav
+ * @version: 1.0
+ * @since: 24-09-06
+ * @description: Utility class for AWS services
+ */
+@Component
 public class AwsUtil {
+    public String getValueFromSecretsManager(
+            String secretName
+    ) {
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(Region.AP_SOUTHEAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+
+        GetSecretValueRequest secretValueRequest =
+                GetSecretValueRequest.builder().secretId(secretName).build();
+
+        GetSecretValueResponse secretValueResponse = null;
+
+        try {
+            secretValueResponse = client.getSecretValue(secretValueRequest);
+        } catch (SdkException e) {
+            throw new RuntimeException("Failed to get secret value", e);
+        }
+
+        if (secretValueResponse == null) {
+            return null;
+        }
+
+        return secretValueResponse.secretString();
+    }
 }
