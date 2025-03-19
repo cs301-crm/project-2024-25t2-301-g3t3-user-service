5b289d893ef44460e35a3d6ea234490a55f05caf
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-19 14:59:18 +0800

    feat: add kafka dev configs

--- src/main/java/com/cs301/crm/configs/KafkaConsumerDevConfig.java
+++ src/main/java/com/cs301/crm/configs/KafkaConsumerDevConfig.java
@@ -1,4 +1,34 @@
 package com.cs301.crm.configs;
 
-public class KafkaConsumerProdConfig {
+import com.amazonaws.services.schemaregistry.serializers.protobuf.ProtobufSerializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+@Profile("dev")
+public class KafkaConsumerDevConfig {
+    @Value("${spring.kafka.bootstrap-servers")
+    private String bootstrapAddress;
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ProtobufSerializer.class.getName());
+
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
 }
