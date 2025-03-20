33e45f6a176e2c7710b458b8ea0c45d47853e2b6
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-20 13:05:26 +0800

    feat: add otp cache

--- src/main/java/com/cs301/crm/configs/OtpConfig.java
+++ src/main/java/com/cs301/crm/configs/OtpConfig.java
@@ -1,4 +1,30 @@
 package com.cs301.crm.configs;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
 public class OtpConfig {
+
+    @Value("${otp.expiration}")
+    private long expirationSeconds;
+
+    @Bean
+    public LoadingCache<String, Integer> otpCache() {
+        return CacheBuilder.newBuilder()
+                .expireAfterWrite(expirationSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<>() {
+                    @NotNull
+                    public Integer load(@NotNull String key) {
+                        return 0;
+                    }
+                });
+    }
 }
