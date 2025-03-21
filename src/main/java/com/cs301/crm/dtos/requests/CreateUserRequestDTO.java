77ab13d1ae8bda6e0da264618ffc808a94c19c7a
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-03-21 18:45:35 +0800

    feat: add notification to user creation

--- src/main/java/com/cs301/crm/dtos/requests/CreateUserRequestDTO.java
+++ src/main/java/com/cs301/crm/dtos/requests/CreateUserRequestDTO.java
@@ -20,12 +20,6 @@ public record CreateUserRequestDTO(
         @Email(message = "Invalid email format")
         String email,
 
-        @NotNull(message = "Password cannot be null")
-        @Size(min = 8, message = "Password must be more than 8 characters")
-        @Pattern(message = "Password must contain 1 symbol, 1 uppercase, 1 lowercase and 1 digit",
-                regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).*$")
-        String password,
-
         @NotNull(message = "Role cannot be null")
         @NotEmpty(message = "Role cannot be empty")
         String userRole
